3f17ae66d1929c45edfbff2722398dee0bdeb511
vrshr <vrshr@LAPTOP-94QVD20J>
2020-06-09 00:30:48 +0530

    added a comment

---
+++ SeleniumPythonByVarsha/MyPyDevPackage/MyPyDevModule.py
@@ -0,0 +1,20 @@
+# '''
+# Created on 30-May-2020
+#  
+# @author: vrshrcks
+# '''
+# from selenium import webdriver
+# # from selenium.webdriver.common.keys import Keys
+# # from webdriver_manager.chrome import ChromeDriverManager
+#  
+#  
+# # driver = webdriver.Chrome(executable_path='D:\chromedriver_win32/chromedriver.exe')
+# # driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+# driver.get("www.myntra.com")
+# #  
+# # element = driver.find_element_by_class_name("desktop-searchBar")
+# # element.send_keys("sports shoes")
+# # element.send_keys(Keys.RETURN)
+#  
+# driver.close()
+###########
